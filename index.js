ec586fa2bbec4ec79fd469b8246442acc9e915ba
shahalam sharif <shahalam.shareef@gmail.com>
2022-05-23 20:54:18 +0600

    server created and installed essential packages

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("server init");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`server is running on port ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
